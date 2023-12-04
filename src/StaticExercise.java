68f7f5b4138b797f76e9fdf33b563f437fc79ea8
GirolaAlejandro <girolaale95@gmail.com>
2023-12-04 11:52:00 +0100

    Static keyword exercise

--- src/StaticExercise.java
+++ src/StaticExercise.java
@@ -1,16 +1,16 @@
 
-    // Static keyword exercise
+// Static keyword exercise
 
-    // 1) Create a new Java class called 'StaticExercise'.
+// 1) Create a new Java class called 'StaticExercise'.
 
-    public class StaticExercise {
+public class StaticExercise {
     // 2) Inside 'StaticExercise', declare a static variable of type int named 'counter'. Initialize it to 0.
     static int counter = 0;
 
     /* 3) Next, create a static method named 'incrementCounter'.
     This method should increase the value of 'counter' by 1 every time it's called.
      */
-     public static void incrementCounter() {
+    public static void incrementCounter() {
         counter++;
     }
     // 4) Create a static block that prints a message "Static block is executed" whenever the class is loaded.
