c41063b884473ccb66c8365fd2032e723866f4e0
GirolaAlejandro <girolaale95@gmail.com>
2023-12-04 11:02:06 +0100

    For Loops Coding Exercise

---
+++ src/LoopsExercise.java
@@ -0,0 +1,18 @@
+
+// Use the loop() function to create a for loop that goes from 0 to 10 and prints all the numbers to the console.
+
+
+public class LoopsExercise {
+    public static void main(String[] args) {
+        loop();
+    }
+
+    public static void loop(){
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+    }
+
+
+
